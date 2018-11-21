d6450c00405662abf1c093313bf2216c6be89cd9
Dom Derrien <dom.derrien@hmhco.com>
2018-11-21 11:51:31 -0500

    Minor optimizations of the HTML rendering

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,8 @@
+export { Content } from './model/Content';
 export { Problem } from './model/Problem';
+export { ProblemResponse } from './model/ProblemResponse';
+export { Variable } from './model/Variable';
 
+export { ContentResource } from './resource/ContentResource';
 export { ProblemResource } from './resource/ProblemResource';
+export { ProblemResponseResource } from './resource/ProblemResponseResource';
